137c1e350f2c8cf90837f24ec094cfeadee5c1d9
Suryanshu07 <45977317+Suryanshusingh123@users.noreply.github.com>
2022-12-06 21:32:50 +0530

    Add files via upload

---
+++ findinganode.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node* next;
+
+        node(int data)
+        {
+            this->data=data;
+            next=NULL;
+        }
+};
+node* takeinput2()
+{
+    int data;
+    cin>>data;
+    node* head=NULL;
+    node* tail=NULL;
+
+    while(data!=-1)
+    {
+        node* newnode=new node(data);
+        if(head==NULL)
+        {
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+                tail->next=newnode;
+                tail=newnode;
+        }
+    cin>>data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+int findelement(node*head,int element)
+{
+    node*temp=head;
+    int count=0;
+   
+    while(temp!=NULL&&element!=temp->data)
+    {
+        temp=temp->next;
+        count++;
+    }
+    if(temp == NULL){
+        return -1;
+    }
+    return count;
+}
+int main()
+{
+    node* head=takeinput2();
+    print(head);
+    int element;
+    cin>>element;
+    cout<<findelement(head,element);
+}
