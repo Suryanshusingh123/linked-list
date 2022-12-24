736e11141af7067262b8c033dca0dd684ecbe8c2
Suryanshu07 <45977317+Suryanshusingh123@users.noreply.github.com>
2022-12-24 21:20:07 +0530

    Add files via upload

---
+++ takeinput2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+    public:
+        int data;
+        node* next;
+
+        node(int data)
+        {
+            this->data=data;
+            next=NULL;
+        }
+};
+node* takeinput2()
+{
+    int data;
+    cin>>data;
+    node* head=NULL;
+    node* tail=NULL;
+
+    while(data!=-1)
+    {
+        node* newnode=new node(data);
+        if(head==NULL)
+        {
+            head=newnode;
+            tail=newnode;
+        }
+        else{
+                tail->next=newnode;
+                tail=newnode;
+        }
+    cin>>data;
+    }
+    return head;
+}
+void print(node *head)
+{
+    node *temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+int main()
+{
+    node* head=takeinput2();
+    print(head);
+}
